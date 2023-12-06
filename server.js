3b337bb4cfd6c0d42afa5c3f5ec13ed63de86220
JM Catamora <1910598john@gmail.com>
2023-12-06 17:17:11 +0800

    commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+  socket.on('message', (msg) => {
+    let data;
+    if (socket.username) {
+      data = {
+        "message": msg,
+        "name" : socket.username,
+      }
+    } else {
+      data = {
+        "message": msg,
+        "name" : "User",
+      }
+    }
+   
+    socket.broadcast.emit('other', data);
+    
+  })
+
+  socket.on("registerName", (name) => {
+    socket.username = name;
+    console.log(socket.username);
+    socket.broadcast.emit("broadcast", name);
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
